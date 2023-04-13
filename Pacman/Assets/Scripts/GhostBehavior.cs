7800532517978d8759ea32651eda555cfd5eaf60
leminhtanan <leminhtanan.01072016@gmail.com>
2023-04-13 12:13:25 +0700

    Them chuc nang eaten pellet va chuc nang quai vat truy duoi player

---
+++ Pacman/Assets/Scripts/GhostBehavior.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Ghost))]
+public abstract class GhostBehavior : MonoBehaviour
+{
+    public Ghost ghost{get; private set;}
+    public float duration;
+    private void Awake() {
+        this.ghost = GetComponent<Ghost>();
+        //this.enabled = false;
+    }
+    public void Enabled(){
+        Enabled(this.duration);
+    }
+    public virtual void Enabled(float duration){
+        this.enabled = true;
+        CancelInvoke();
+        Invoke(nameof(Disable), duration);
+    }
+    public virtual void Disable(){
+        this.enabled = false;
+        CancelInvoke();
+    }
+}
